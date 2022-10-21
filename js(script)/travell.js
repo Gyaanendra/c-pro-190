28904667fe9cbfddeb07aa7926cdd0161ced5893
Gyaanendra <74192896+Gyaanendra@users.noreply.github.com>
2022-10-21 08:18:06 +0530

    stage3

---
+++ js(script)/travell.js
@@ -0,0 +1,559 @@
+const q = [
+  {
+  "worldQuiz": {
+      "taj": {
+          "questions": {
+              "q1": {
+                  "Ques": "where are you?",
+                  "option1": "Taj Mahal",
+                  "option2": "Red fort",
+                  "option3": "new york",
+                  "option4": "china",
+                  "answer": "Taj Mahal"
+              },
+              "q2": {
+                  "Ques": "In which state of India is the Taj Mahal situated?",
+                  "option1": "Maharashtra",
+                  "option2": "Uttar Pradesh",
+                  "option3": "Uttarakhand",
+                  "option4": "Madhya Pradesh",
+                  "answer": "Uttar Pradesh"
+              },
+              "q3": {
+                  "Ques": " Who was the chief architect of the Taj Mahal?",
+                  "option1": "Shah Jahan",
+                  "option2": "Ustad Aḥmad Lahawrī",
+                  "option3": "Ustad Bismillah Khan",
+                  "option4": "Ustad Amjad Ali Khan",
+                  "answer": "Ustad Aḥmad Lahawrī"
+              },
+              "q4": {
+                  "Ques": "How many years did it take to complete the complex?",
+                  "option1": "Approx 20 years",
+                  "option2": "Approx 21 years",
+                  "option3": "Approx 22 years",
+                  "option4": "Approx 23 years",
+                  "answer": "Approx 22 years"
+              },
+              "q5": {
+                  "Ques": "When was a restoration and research program for the Taj Mahal initiated?",
+                  "option1": "1990",
+                  "option2": "2007",
+                  "option3": "2024",
+                  "option4": "1998",
+                  "answer": "1998"
+              },
+              "q6": {
+                  "Ques": "When was the Taj Mahal designated a UNESCO World Heritage Site?",
+                  "option1": "1983",
+                  "option2": "456",
+                  "option3": "2020",
+                  "option4": "2018",
+                  "answer": "1983"
+              },
+              "q7": {
+                  "Ques": "Which of the following is/are the principal element of the Taj Mahal complex?",
+                  "option1": "Mosque",
+                  "option2": "Jawāb",
+                  "option3": "Main gateway",
+                  "option4": "All the above",
+                  "answer": "All the above"
+              }
+          }
+      },
+      "rio": {
+          "questions": {
+              "q1": {
+                  "Ques": "where are you?",
+                  "option1": "Christ the Redeemer",
+                  "option2": "Rio",
+                  "option3": "愚か者",
+                  "option4": "ідіот",
+                  "answer": "Christ the Redeemer"
+              },
+              "q2": {
+                  "Ques": "The statue was completed in what year?",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q3": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q4": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q5": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q6": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q7": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q8": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q9": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q10": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              }
+          }
+      },
+      "pyramids": {
+          "questions": {
+              "q1": {
+                  "Ques": "where are you?",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q2": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q3": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q4": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q5": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q6": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q7": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q8": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q9": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q10": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              }
+          }
+      },
+      "petra-jordan": {
+          "questions": {
+              "q1": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q2": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q3": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q4": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q5": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q6": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q7": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q8": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q9": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q10": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              }
+          }
+      },
+      "gwc": {
+          "questions": {
+              "q1": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q2": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q3": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q4": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q5": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q6": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q7": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q8": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q9": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              },
+              "q10": {
+                  "Ques": "",
+                  "option1": "",
+                  "option2": "",
+                  "option3": "",
+                  "option4": "",
+                  "answer": ""
+              }
+          }
+      }
+  }
+}
+]
+
+
+
+AFRAME.registerComponent("tour", {
+    schema:{
+      state:{type:"string",default:"places-list"},
+      selected_card:{type:"string",default:"#card1"}
+    },
+    init: function () {
+      this.placesContainer = this.el;
+      this.createCards();
+    },
+    tick:function(){
+      const{state} =this.el.getAttribute("tour")
+      if (state === "view"){
+        this.hideEl([this.placesContainer])
+        this.show_view()
+      }
+    },
+  
+    createCards: function () {
+      const thumbNailsRef = [
+        {
+          id: "taj",
+          title: "Taj Mahal",
+          url: "./taj/55000481_303.jpg",
+        },
+        {
+          id: "petra-jordan",
+          title: "petra-jordan",
+          url: "../petra-jordan/AUC_3062-2.jpg",
+        },
+  
+        {
+          id: "gwc",
+          title: "gwc",
+          url: "../gwc/gwc1.jpg",
+        },
+        {
+          id: "pyramids",
+          title: "pyramids",
+          url: "../pyramids/Kheops-Pyramid.jpg",
+        },
+        {
+          id: "rio",
+          title: "rio",
+          url: "./rio/rio_christ_vr_01_big.jpg",
+        },
+      ];
+      let prevoiusXPosition = -60;
+  
+      for (var item of thumbNailsRef) {
+        const posX = prevoiusXPosition + 25;
+        const posY = 10;
+        const posZ = -40;
+        const position = { x: posX, y: posY, z: posZ };
+        prevoiusXPosition = posX;
+  
+        // Border Element
+        const borderEl = this.createBorder(position, item.id);
+  
+        // Thumbnail Element
+        const thumbNail = this.createThumbNail(item);
+        borderEl.appendChild(thumbNail);
+  
+        // Title Text Element
+        const titleEl = this.createTitleEl(position, item);
+        borderEl.appendChild(titleEl);
+
+        const qp = this.createQuestion(position);
+        borderEl.appendChild(qp)
+
+        this.placesContainer.appendChild(borderEl);
+      }
+    },
+    createBorder: function (position, id) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("id", id);
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("geometry", {
+        primitive: "ring",
+        radiusInner: 9,
+        radiusOuter: 10,
+      });
+      entityEl.setAttribute("position", position);
+      entityEl.setAttribute("material", {
+        color: "#0077CC",
+        opacity: 1,
+      });
+  
+      //Add cursor-listener component to the ring border entity to change it's color 
+      //On Cursor 'mouseenter' and 'mouseleave' entity
+      entityEl.setAttribute("cursor-listener", {});
+  
+      return entityEl;
+    },
+    createThumbNail: function (item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("geometry", {
+        primitive: "circle",
+        radius: 9,
+      });
+      entityEl.setAttribute("material", { src: item.url });
+  
+      return entityEl;
+    },
+    createTitleEl: function (position, item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("text", {
+        font: "exo2bold",
+        align: "center",
+        width: 70,
+        color: "#e65100",
+        value: item.title,
+      });
+      const elPosition = position;
+      elPosition.y = -20;
+      entityEl.setAttribute("position", elPosition);
+      entityEl.setAttribute("visible", true);
+      
+      return entityEl;
+    },
+    hideEl:function(el_list){
+      el_list.map((el)=>{
+        el.setAttribute("visible",false)
+      })
+    },
+    show_view:function(){
+      const {selected_card} = this.data;
+      const sky_el = document.querySelector("#main-container")
+      sky_el.setAttribute("material",{src:`./assets/360_images/${selected_card}/p0.jpg`,color:"white"})
+    },
+
+    createQuestion: function(position){
+      for(var ques of q ){
+        const q = ques.worldQuiz
+        const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("text", {
+        font: "exo2bold",
+        align: "center",
+        width: 70,
+        margin:10,
+        color: "#e65100",
+        value: q.id.questions.q1,
+      });
+      const elPosition = position;
+      elPosition.y = -20;
+      entityEl.setAttribute("position", elPosition);
+      entityEl.setAttribute("visible", true);
+      
+      }
+    }
+    
+  });
+
+  
